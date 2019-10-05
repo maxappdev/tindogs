e32c0e299ce7e8198d3637974aff68fde343fb7a
maxappdev <maxym.oliynyk95@gmail.com>
2019-10-05 21:59:37 +0200

    intrested_person api added

--- app/Http/Controllers/InterestedPeople.php
+++ app/Http/Controllers/InterestedPeople.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Dog;
 use App\Doglooker;
 use App\Interest;
+use App\User;
 
 
 class InterestedPeople extends Controller
@@ -13,6 +14,18 @@ class InterestedPeople extends Controller
     public function index($dog_id){
         $allInterestsIds = Interest::where("dog_id", $dog_id)->pluck("doglooker_id");
         $dogLookers = Doglooker::whereIn('id',$allInterestsIds)->get();
+
+        $doglookers = json_encode($dogLookers);
+
+        foreach($dogLookers as $dogLooker){
+
+            $user = User::find($dogLooker->user_id);
+            $dogLooker->name = $user->name;
+            $dogLooker->phone = $user->phone;
+            $dogLooker->address = $user->address;
+
+        }
+
         return response()->json($dogLookers);
     }
 }
